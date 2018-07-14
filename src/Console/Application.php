ec8fbc443b83be273a8024db2d4bed0b8535a572
Chi-teck <Chi-teck@users.noreply.github.com>
2018-07-14 16:33:52 +0500

    Implement single command application.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -5,6 +5,7 @@ namespace Allocine\Twigcs\Console;
 use Allocine\Twigcs\Container;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArgvInput;
 
 class Application extends BaseApplication
 {
@@ -22,7 +23,17 @@ class Application extends BaseApplication
         parent::__construct($name, $version);
 
         $this->container = new Container();
-        $this->add(new LintCommand());
+        $command = new LintCommand();
+        $this->add($command);
+        // Support old way to execute linter (`twigcs lint <path>`) to preserve
+        // backward compatibility.
+        if ((new ArgvInput())->getFirstArgument() == 'lint') {
+            @trigger_error("Calling 'lint' command is deprecated. Run `twigs <path>` instead.", E_USER_DEPRECATED);
+        }
+        else {
+            $this->setDefaultCommand($command->getName(), true);
+        }
+
     }
 
     /**
