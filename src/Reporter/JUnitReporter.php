913c7333f3a0875343923d78f9a63aa381dea402
Chi-teck <Chi-teck@users.noreply.github.com>
2018-10-03 09:53:43 +0500

    Create testsuite per file

--- src/Reporter/JUnitReporter.php
+++ src/Reporter/JUnitReporter.php
@@ -11,17 +11,26 @@ class JUnitReporter implements ReporterInterface
      */
     public function report(OutputInterface $output, array $violations)
     {
+        $filename = null;
+        $testsuite = null;
+        $totalFailures = 0;
+
         $testsuites = new \SimpleXMLElement('<testsuites name="Twigcs"/>');
 
         foreach ($violations as $violation) {
-            $testsuite = $testsuites->addChild('testsuite');
-            $testsuite->addAttribute('name', $violation->getFilename());
-            $testcase = $testsuites->addChild('testcase');
+            if ($filename != $violation->getFilename()) {
+              $filename = $violation->getFilename();
+              $testsuite = $testsuites->addChild('testsuite');
+              $testsuite->addAttribute('name', $filename);
+              $totalFailures = 0;
+            }
+            $testsuite['failures'] = ++$totalFailures;
+            $testcase = $testsuite->addChild('testcase');
             $testcase->addAttribute('name', $violation->getSource());
             $failure = $testcase->addChild('failure');
-            $position = '(' . $violation->getLine() . ':' . $violation->getColumn() . ')';
+            $failure[0] = $violation->getReason()  . ' at (' . $violation->getLine() . ':' . $violation->getColumn() . ')';
             $failure->addAttribute('type', 'error');
-            $failure->addAttribute('message', $violation->getReason()  . ' at ' . $position);
+            $failure->addAttribute('message', $failure[0]);
         }
 
         $output->writeln($testsuites->asXML());
