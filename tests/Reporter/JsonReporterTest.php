ac1c6d86795f06a7dcce5640b32b7b7a2fb7d983
Laurent Muller <laurent.muller@haslerrail.com>
2020-02-07 15:15:19 +0100

    Added a test for JsonReport with 2 different files.

--- tests/Reporter/JsonReporterTest.php
+++ tests/Reporter/JsonReporterTest.php
@@ -28,6 +28,38 @@ const EXPECTED_REPORT = <<<EOF
 }
 EOF;
 
+const EXPECTED_REPORT_MULTIPLE = <<<EOF
+{
+    "failures": 2,
+    "files": [
+        {
+            "file": "first.twig",
+            "violations": [
+                {
+                    "line": 1,
+                    "column": 2,
+                    "severity": 3,
+                    "type": "error",
+                    "message": "This is the first file."
+                }
+            ]
+        },
+        {
+            "file": "second.twig",
+            "violations": [
+                {
+                    "line": 10,
+                    "column": 20,
+                    "severity": 1,
+                    "type": "info",
+                    "message": "This is the second file."
+                }
+            ]
+        }
+    ]
+}
+EOF;
+
     public function testReport()
     {
         $reporter = new JsonReporter();
@@ -45,6 +77,27 @@ EOF;
 
         $reporter->report($output, [
             new Violation('template.twig', 10, 20, 'You are not allowed to do that.'),
+        ]);
+    }
+
+    public function testReportMultiple()
+    {
+        $reporter = new JsonReporter();
+        $output = $this
+            ->getMockBuilder('Symfony\Component\Console\Output\ConsoleOutput')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $output
+            ->expects($this->once())
+            ->method('writeln')
+            ->with(self::EXPECTED_REPORT_MULTIPLE)
+        ;
+
+        $reporter->report($output, [
+            new Violation('first.twig', 1, 2, 'This is the first file.'),
+            new Violation('second.twig', 10, 20, "This is the second file.", Violation::SEVERITY_INFO),
         ]);
     }
 }
