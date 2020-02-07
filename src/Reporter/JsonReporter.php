a9d462acb6091dd8af6bb5a258da185a753b0936
Laurent Muller <laurent.muller@haslerrail.com>
2020-02-07 11:49:44 +0100

    Added Json Reporter

---
+++ src/Reporter/JsonReporter.php
@@ -0,0 +1,40 @@
+<?php
+namespace FriendsOfTwig\Twigcs\Reporter;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use FriendsOfTwig\Twigcs\Validator\Violation;
+
+class JsonReporter implements ReporterInterface {
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param Violation[] $violations
+     */
+    public function report(OutputInterface $output, array $violations)
+    {
+        $filename = null;
+
+        $result = new \stdClass();
+        $result->failures = count($violations);
+
+        foreach ($violations as $violation) {
+            if ($filename !== $violation->getFilename()) {
+                $filename = $violation->getFilename();
+                $entry = new \stdClass();
+                $entry->file = $filename;
+                $result->files[] = $entry;
+            }
+
+            $entry->violations[] = [
+                'line' => $violation->getLine(),
+                'column' => $violation->getColumn(),
+                'severity' => $violation->getSeverity(),
+                'type' => strtolower($violation->getSeverityAsString()),
+                'message' => $violation->getReason(),
+            ];
+        }
+
+        $output->writeln(json_encode($result, JSON_PRETTY_PRINT));
+    }
+}
