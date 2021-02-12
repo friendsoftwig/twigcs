5a48c9f87d3771d4a77e30f0d29cd977ed145f8c
Jimmy ESCRICH <jescrich@ruedesvignerons.com>
2021-02-12 17:58:59 +0100

    Review @stof
    Signed-off-by: Jimmy ESCRICH <jescrich@ruedesvignerons.com>

--- src/Reporter/GithubActionReporter.php
+++ src/Reporter/GithubActionReporter.php
@@ -7,18 +7,38 @@ use Symfony\Component\Console\Output\OutputInterface;
 class GithubActionReporter implements ReporterInterface
 {
     /**
+     * @see https://github.com/actions/toolkit/blob/5e5e1b7aacba68a53836a34db4a288c3c1c1585b/packages/core/src/command.ts#L80-L85
+     */
+    private const ESCAPED_DATA = [
+        '%' => '%25',
+        "\r" => '%0D',
+        "\n" => '%0A',
+    ];
+
+    /**
+     * @see https://github.com/actions/toolkit/blob/5e5e1b7aacba68a53836a34db4a288c3c1c1585b/packages/core/src/command.ts#L87-L94
+     */
+    private const ESCAPED_PROPERTIES = [
+        '%' => '%25',
+        "\r" => '%0D',
+        "\n" => '%0A',
+        ':' => '%3A',
+        ',' => '%2C',
+    ];
+
+    /**
      * {@inheritdoc}
      */
     public function report(OutputInterface $output, array $violations)
     {
-        foreach ($violations as $violation) {
+        foreach ($violations as $key => $violation) {
             $output->writeln(sprintf(
-                '::%s file=%s, line=%d, col=%d::%s',
+                '::%s file=%s, line=%s, col=%s::%s',
                 strtolower($violation->getSeverityAsString()),
-                $violation->getFilename(),
-                $violation->getLine(),
-                $violation->getColumn(),
-                $violation->getReason()
+                strtr($violation->getFilename(), self::ESCAPED_PROPERTIES),
+                strtr($violation->getLine() ?? 1, self::ESCAPED_PROPERTIES),
+                strtr($violation->getColumn() ?? 0, self::ESCAPED_PROPERTIES),
+                strtr($violation->getReason(), self::ESCAPED_DATA)
             ));
         }
     }
